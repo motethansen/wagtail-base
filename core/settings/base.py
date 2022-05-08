bef9d07724fbcdedbedd3c9151e28d9104241147
Michael Hansen <mm@lesmotet.com>
2022-05-08 20:16:47 +0800

    added simple modules for home page

--- core/settings/base.py
+++ core/settings/base.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'wagtail.search',
     'wagtail.admin',
     'wagtail.core',
-
+    #'gallery',
     'modelcluster',
     'taggit',
 
--- home/models.py
+++ home/models.py
@@ -1,8 +1,25 @@
 from django.db import models
 
 from wagtail.core.models import Page
-
+from wagtail.core.fields import RichTextField
+from wagtail.admin.edit_handlers import FieldPanel, RichTextField
+#from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
 
 class HomePage(Page):
     """Home page model"""
     templates = "home/home_page.html"
+    header =''
+    banner_title = models.CharField(max_length=100,blank=False,null=True)
+    #header = models.CharField("Header", max_length=200)
+    body = RichTextField(blank=True)
+    #image = models.ForeignKey('wagtailimages.Image', related_name='+', on_delete=models.CASCADE)
+    content_panels = Page.content_panels + [ 
+    #     FieldPanel('header'),
+    #     ImageChooserPanel('image'),
+         FieldPanel("banner_title"),
+         FieldPanel('body', classname="full"),
+    #     FieldPanel("banner_title")
+    ]
+
+    class Meta:
+        verbose_name = "home page"
